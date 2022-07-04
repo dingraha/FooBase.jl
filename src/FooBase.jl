17e349eefeb93e5895f2ede0a89e5dc835d08ada
Daniel Ingraham <d.j.ingraham@gmail.com>
2022-07-04 17:24:37 -0400

    Add eat method

--- src/FooBase.jl
+++ src/FooBase.jl
@@ -3,6 +3,7 @@ module FooBase
 abstract type AbstractAnimal end
 
 say_hello(animal::AbstractAnimal) = println("I am an abstract animal!")
+eat(animal::AbstractAnimal) = println("AbstractAnimal is eating.")
 
 other_function() = println("hello from FooBase.other_function")
 
